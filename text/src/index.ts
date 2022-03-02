f3d4f53514b5dd733fb07c6c262b4366ca2d093a
J. Ryan Stinnett <jryans@gmail.com>
2022-03-02 12:31:01 +0000

    Expose state for debugging

--- text/src/index.ts
+++ text/src/index.ts
@@ -17,7 +17,7 @@ limitations under the License.
 import { EditorState } from "prosemirror-state";
 import { EditorView } from "prosemirror-view";
 import { Schema, DOMParser } from "prosemirror-model";
-import { schema } from "prosemirror-schema-basic";
+import { schema as basicSchema } from "prosemirror-schema-basic";
 import { addListNodes } from "prosemirror-schema-list";
 import { exampleSetup } from "prosemirror-example-setup";
 import { WidgetApi } from "matrix-widget-api";
@@ -28,18 +28,20 @@ import "./index.css";
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
-const mySchema = new Schema({
-    nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-    marks: schema.spec.marks
+const schema = window.schema = new Schema({
+    nodes: addListNodes(basicSchema.spec.nodes, "paragraph block*", "block"),
+    marks: basicSchema.spec.marks
 });
 
-const view = new EditorView(document.querySelector("#editor") as Element, {
-    state: EditorState.create({
-        doc: DOMParser.fromSchema(mySchema).parse(
-            document.querySelector("#content") as Element,
-        ),
-        plugins: exampleSetup({ schema: mySchema }),
-    }),
+const content = document.querySelector("#content") as Element;
+const state = window.state = EditorState.create({
+    doc: DOMParser.fromSchema(schema).parse(content),
+    plugins: exampleSetup({ schema }),
+});
+
+const editor = document.querySelector("#editor") as Element;
+const view = window.view = new EditorView(editor, {
+    state,
     dispatchTransaction(transaction) {
         console.log(transaction);
         const newState = view.state.apply(transaction)
